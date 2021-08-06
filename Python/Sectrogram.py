5984fefedc1db4b53ae052980ce5f374c8fd5bec
Mahdieh Pirmoradian <74245613+MahdiehPirmoradian@users.noreply.github.com>
2021-08-06 13:52:22 +0200

    Add files via upload

--- Python/Sectrogram.py
+++ Python/Sectrogram.py
@@ -1,10 +1,13 @@
 # read excell file
+import matplotlib.pyplot
 import  numpy as np
 import matplotlip as matplotlip
+
 import matplotlib.pyplot as plt
 import openpyxl
 df = openpyxl.load_workbook("Data Object 2.xlsx")
 
+import os
 
 
 sheet= df.active
@@ -19,15 +22,29 @@ print(rows, columns)
 
 print(df)
 lista = []
-for j in range(1,row):
-    for i in range(1,col):
-        e = sheet.cell(row=j, column=i)
-        lista.append(e.value)
-    plt.plot(lista)
-    plt.show()
-    lista = []
-    
+listb = []
+for k in range(1,4):
+    for j in range(1,row):
+        for i in range(1,col):
+            e = sheet.cell(row=j, column=i)
+            lista.append(e.value)
+            listb.append(e.value)
+        plt.plot(lista)
+        plt.title(str(j))
+        plt.show()
+        lista = []
+        plt.specgram(listb)
+        plt.show()
+        listb = []
+        #plt_spec = plt.make_spectrum(lista)
+        #plt_spec.plot()
+        #plt_spec.show()
 
+
+    directory = "object" + str(k)
+    parent_dir = "D:\FRA-UAS\Sem2\CompInt-Project-T3\Python"
+    path = os.path.join(parent_dir, directory)
+    os.mkdir(path)
 
 
 
