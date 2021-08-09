55f056775055cb12da2bf89026770d79827d6576
Mahdieh Pirmoradian <74245613+MahdiehPirmoradian@users.noreply.github.com>
2021-08-10 01:12:02 +0200

    Add files via upload

--- Python/Sectrogram.py
+++ Python/Sectrogram.py
@@ -1,31 +1,30 @@
 # read excell file
 import matplotlib.pyplot
-import  numpy as np
+import numpy as np
 import matplotlip as matplotlip
-
+import pandas as pd
 import matplotlib.pyplot as plt
 import openpyxl
-df = openpyxl.load_workbook("Data Object 2.xlsx")
-
+#os is used to list the files in a directory
 import os
 
 
-sheet= df.active
-rows = sheet.max_row
-row = rows + 1
-columns = sheet.max_column
-col = columns+1
-print(rows, columns)
 
-#import pandas as pd
-#df = pd.read_excel('Data Object 2.xlsx')
+#print(df)
 
-print(df)
-lista = []
-listb = []
 for k in range(1,4):
-    for j in range(1,row):
-        for i in range(1,col):
+    df = openpyxl.load_workbook("Data Object " + str(k)+".xlsx")
+    #df = openpyxl.load_workbook("Data Object 1.xlsx")
+    sheet = df.active
+    rows = sheet.max_row
+    row = rows + 1
+    columns = sheet.max_column
+    col = columns + 1
+    print(rows, columns)
+    lista = []
+    listb = []
+    for j in range(1, row):
+        for i in range(1, col):
             e = sheet.cell(row=j, column=i)
             lista.append(e.value)
             listb.append(e.value)
@@ -34,6 +33,7 @@ for k in range(1,4):
         plt.show()
         lista = []
         plt.specgram(listb)
+        plt.title(str(j))
         plt.show()
         listb = []
         #plt_spec = plt.make_spectrum(lista)
