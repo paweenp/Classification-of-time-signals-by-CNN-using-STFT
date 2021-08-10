d240e7e12f4f213d220e389f6faab336c01a0e38
Mahdieh Pirmoradian <74245613+MahdiehPirmoradian@users.noreply.github.com>
2021-08-10 16:56:44 +0200

    Add files via upload
    save both time and frequency domain plots

--- Python/Sectrogram.py
+++ Python/Sectrogram.py
@@ -1,11 +1,12 @@
 # read excell file
-import matplotlib.pyplot
+
 import numpy as np
-import matplotlip as matplotlip
+import matplotlib
+#import matplotlib as matplotlib
 import pandas as pd
 import matplotlib.pyplot as plt
 import openpyxl
-#os is used to list the files in a directory
+#os is used to work with files & directories
 import os
 
 
@@ -24,27 +25,49 @@ for k in range(1,4):
     lista = []
     listb = []
     directory = "object" + str(k)
-    parent_dir = "D:\FRA-UAS\Sem2\CompInt-Project-T3\Python"
+    parent_dir = os.getcwd()
     path = os.path.join(parent_dir, directory)
     os.mkdir(path)
+    os.chdir(path)
+
     for j in range(1, row):
         for i in range(1, col):
             e = sheet.cell(row=j, column=i)
             lista.append(e.value)
             listb.append(e.value)
+        folder = "Time_Domain"
+        cur_dir = os.getcwd()
+        cur_path = os.path.join(cur_dir, folder)
+        # dir_exist = os.path.exists(curpath)
+        # dir_exist = True
+        if not os.path.exists(cur_path):
+            os.mkdir(cur_path)
+        os.chdir(cur_path)
         plt.plot(lista)
-        plt.title(str(j))
-        plt.show()
+        NOP = str(j)
+        plt.title(NOP)
+        #plt.show()
+        plt.savefig(NOP + ".jpg", bbox_inches="tight")
+        plt.clf()
         lista = []
+        os.chdir(path)
+        folder = "Frequency_Domain"
+        curdir = os.getcwd()
+        curpath = os.path.join(curdir, folder)
+        if not os.path.exists(curpath):
+            os.mkdir(curpath)
+        os.chdir(curpath)
         plt.specgram(listb)
-        plt.title(str(j))
-        plt.show()
+        plt.title(NOP)
+        plt.savefig(NOP + ".jpg", bbox_inches="tight")
+        plt.clf()
         listb = []
+        os.chdir(path)
         #plt_spec = plt.make_spectrum(lista)
         #plt_spec.plot()
         #plt_spec.show()
 
-
+    os.chdir(parent_dir)
 
 
 
